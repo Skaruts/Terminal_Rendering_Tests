4e43278e3c53e19ab59f6f79b415aff2f70d8b48
Skaruts <cedomingos@gmail.com>
2020-01-27 10:35:55 +0000

    remove benchmark

--- src/map_gdn.nim
+++ src/map_gdn.nim
@@ -1,15 +1,17 @@
-import random, times, strutils
-# import nimex   # <-- xrange, etc
+import random
 import godot, godotapi/[node_2d]
 import gdw, game_camera, terminal
 
-template benchmark(benchmarkName: string, code: untyped) =
-    block:
-        let t0 = epochTime()
-        code
-        let elapsed = epochTime() - t0
-        let elapsedStr = elapsed.formatFloat(format = ffDecimal, precision = 3)
-        echo "    CPU Time: ", elapsedStr, "s\n"
+
+# import times
+# import strutils
+# template benchmark(benchmarkName: string, code: untyped) =
+#     block:
+#         let t0 = epochTime()
+#         code
+#         let elapsed = epochTime() - t0
+#         let elapsedStr = elapsed.formatFloat(format = ffDecimal, precision = 3)
+#         echo "    CPU Time: ", elapsedStr, "s\n"
 
 
 const FLOOR = 0
@@ -37,22 +39,22 @@ gdobj MapGDN of Node2D:
         return self.tilemap[x+y*self.w] != WALL
 
     proc update_stuff*(terminal:Terminal, cam:GameCameraGDN) {.gdExport.} =
-        benchmark "":
-            for j in 0..<cam.h:
-                for i in 0..<cam.w:
-                    let mx = cam.mx + i
-                    let my = cam.my + j
+        # benchmark "":
+        for j in 0..<cam.h:
+            for i in 0..<cam.w:
+                let mx = cam.mx + i
+                let my = cam.my + j
 
-                    if mx >= 0 and my >= 0 and mx < self.w and my < self.h:
-                        let p = cam.to_cam_coords(mx, my)
+                if mx >= 0 and my >= 0 and mx < self.w and my < self.h:
+                    let p = cam.to_cam_coords(mx, my)
 
-                        if cam.contains(p):
-                            if self.tilemap[mx+my*self.w] == WALL:
-                                terminal.set_fg(wall_color)
-                                terminal.put(int(p.x), int(p.y), 35)
-                            else:
-                                terminal.set_fg(floor_color)
-                                terminal.put(int(p.x), int(p.y), 249)
+                    if cam.contains(p):
+                        if self.tilemap[mx+my*self.w] == WALL:
+                            terminal.set_fg(wall_color)
+                            terminal.put(int(p.x), int(p.y), 35)
+                        else:
+                            terminal.set_fg(floor_color)
+                            terminal.put(int(p.x), int(p.y), 249)
 
 
 
